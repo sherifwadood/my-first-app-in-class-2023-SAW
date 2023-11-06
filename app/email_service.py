ae8240d7ce191d471d35fc87bcde8396f197c1c2
sherifwadood <sma337@georgetown.edu>
2023-11-06 14:12:12 -0500

    send to user email

--- app/email_service.py
+++ app/email_service.py
@@ -35,6 +35,10 @@ def send_email(recipient_address=SENDER_ADDRESS, subject="[Shopping Cart App] Te
         print(type(err))
         print(err)
 
+
+
+user_address = input("Please enter your email address: ")
+
 my_content = """
 
     <img
@@ -53,4 +57,4 @@ my_content = """
         <li>Strawberry</li>
     </ul>
 """
-send_email(html_content=my_content)
+send_email(html_content=my_content, recipient_adress=user_address)
