8b67765d789ee9c862d057659d8a8b58a4a1b5ca
sherifwadood <sma337@georgetown.edu>
2023-12-01 18:41:22 -0500

    fixing my thing back

--- app/unemployment.py
+++ app/unemployment.py
@@ -1,12 +1,16 @@
+
+# IMPORTS AT THE TOP
+
+import os
 import json
 from pprint import pprint
 from statistics import mean
 
+from dotenv import load_dotenv
 import requests
 from plotly.express import line
 
-from app.alpha import API_KEY
-
+from app.email_service import send_email
 
 def format_pct(my_number):
     """
@@ -18,13 +22,22 @@ def format_pct(my_number):
     """
     return f"{my_number:.2f}%"
 
+print("BACK IN UNEMPLOYMENT FILE")
 
 
-def fetch_unemployment_data():
-    """Fetches unemployment data from the AlphaVantage API.
-        Returns data as a list of dictionaries, where each represents the unemployment rate for a given month.
-        Formats rates as floats.
-    """
+# ENVIRONMENT VARIABLES AND CONSTANTS
+
+load_dotenv() # go look in the .env file for any env vars
+
+API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
+
+#breakpoint()
+
+#quit()
+
+# FUNCTIONS
+
+def fetch_data():
 
     request_url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={API_KEY}"
 
@@ -32,27 +45,20 @@ def fetch_unemployment_data():
 
     parsed_response = json.loads(response.text)
     print(type(parsed_response))
-    pprint(parsed_response)
+    print(parsed_response.keys())
+    #pprint(parsed_response)
 
     data = parsed_response["data"]
-    #return data
-
-    # we could return the raw data, but the values are strings,
-    # so let's convert them to floats (more usable) before returning
-    for d in data:
-        d["value"] = float(d["value"]) # this is mutating and will overwrite the data
-
     return data
-
 
 
 
 if __name__ == "__main__":
 
 
+    # WORKING CODE
 
-    data = fetch_unemployment_data()
-
+    data = fetch_data()
 
     # Challenge A
     #
@@ -62,29 +68,53 @@ if __name__ == "__main__":
     print("-------------------------")
     print("LATEST UNEMPLOYMENT RATE:")
     #print(data[0])
-    print(f"{format_pct(data[0]['value'])}", "as of", data[0]["date"])
+
+
+    latest_rate = data[0]['value']
+    latest_date = data[0]["date"]
+
+    print(f"{latest_rate}%", "as of", latest_date)
+
 
     # Challenge B
     #
     # What is the average unemployment rate for all months during this calendar year?
     # ... How many months does this cover?
 
+
     this_year = [d for d in data if "2023-" in d["date"]]
 
-    rates_this_year = [d["value"] for d in this_year]
+    rates_this_year = [float(d["value"]) for d in this_year]
     #print(rates_this_year)
 
     print("-------------------------")
-    print("AVG UNEMPLOYMENT THIS YEAR:", f"{format_pct(mean(rates_this_year))}")
+    print("AVG UNEMPLOYMENT THIS YEAR:", f"{round(mean(rates_this_year), 2)}%")
     print("NO MONTHS:", len(this_year))
-
 
     # Challenge C
     #
     # Plot a line chart of unemployment rates over time.
 
+
     dates = [d["date"] for d in data]
-    rates = [d["value"] for d in data]
+    rates = [float(d["value"]) for d in data]
 
     fig = line(x=dates, y=rates, title="United States Unemployment Rate over time", labels= {"x": "Month", "y": "Unemployment Rate"})
     fig.show()
+
+    # EMAIL SENDING
+
+
+
+
+    user_address = input("Please enter your email address: ")
+
+
+
+    content = f"""
+    <h1> Unemployment Report Email </h1>
+
+    <p> Latest rate: {latest_rate}% as of {latest_date} </p>
+    """
+
+    send_email(recipient_address=user_address, html_content=content, subject="Your Unemployment Report")
